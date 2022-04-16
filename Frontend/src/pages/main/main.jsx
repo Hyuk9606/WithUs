6d253c3a4fd84f74eebcf4a345df2b88e0a51546
youngmin <kgjdudals@naver.com>
2022-04-16 22:22:03 +0900

    feat : 메인화면 동영상 자동재생 구현

--- Frontend/src/pages/main/main.jsx
+++ Frontend/src/pages/main/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import Navbar from '../../component/navbar';
+import styled from 'styled-components';
+import test from '../../video/test.mp4'
+
+const MainContainer = styled.div`
+  margin-top: 76px ;
+`
+const BgVideo = styled.video`
+  
+`
+
 
 export default function Main() {
   return (
     <>
-      <div>main</div>
+      <MainContainer>
+        <video
+          autoPlay
+          muted
+          loop
+          width="100%"
+        >
+          <source src={test} type='video/mp4'/>
+        </video>
+      </MainContainer>
       <Navbar />
     </>
   )
