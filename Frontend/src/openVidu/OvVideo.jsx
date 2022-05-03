50e9551c79817d1671d33a01aa0dbe266bbc251c
재현 <jungjh122@naver.com>
2022-05-03 15:00:32 +0900

    feat: openvidu mic 껐음소거 기능 추가

--- Frontend/src/openVidu/OvVideo.jsx
+++ Frontend/src/openVidu/OvVideo.jsx
@@ -7,35 +7,20 @@ export default class OpenViduVideoComponent extends Component {
         this.videoRef = React.createRef();
     }
 
-    componentDidUpdate() {
-        if (this.props && !!this.videoRef) {
+    componentDidUpdate(props) {
+        if (props && !!this.videoRef) {
             this.props.streamManager.addVideoElement(this.videoRef.current);
         }
-
     }
 
     componentDidMount() {
         if (this.props && !!this.videoRef) {
             this.props.streamManager.addVideoElement(this.videoRef.current);
         }
-        console.log("==========user", this.props)
-        // if (this.props && this.props.user.streamManager.session && this.props.user && !!this.videoRef) {
-        //     this.props.user.streamManager.session.on('signal:userChanged', (event) => {
-        //         const data = JSON.parse(event.data);
-        //         console.log("이벤트 갑지 =========", data)
-        //         if (data.isScreenShareActive !== undefined) {
-        //             this.props.user.getStreamManager().addVideoElement(this.videoRef.current);
-        //         }
-        //     });
-        // }
     }
 
     render() {
-        return <video
-            autoPlay={true}
-            ref={this.videoRef}
-            muted={this.props.mutedSound}
-        />;
+        return <video autoPlay={true} ref={this.videoRef} />;
     }
 
 }
